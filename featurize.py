ff1314a7c210fe640a59708796b929f358ded57c
Khalory <tk100794@gmail.com>
2016-12-22 20:38:33 +0000

    Updated code massively; implementation complete. Included various generated images from the tests

--- featurize.py
+++ featurize.py
@@ -1,22 +1,51 @@
 #Featurize
-from sklearn.decomposition import MiniBatchDictionaryLearning
-import patches
+#from sklearn.decomposition import FastICA
+from sklearn.decomposition import PCA
+import patcher
 import numpy as np
+
+numGlobalFeatures = 5
+numComponents = 40
+numFeatures = numComponents + numGlobalFeatures
+
+def flattenFirst(arr):
+	flat = np.zeros((len(arr[:])*len(arr[0][:]), len(arr[0][0][:])))
+	for i, x in enumerate(arr):
+		for j, y in enumerate(x):
+			#print(y)
+			flat[i*len(x)+j] = flatten(y)
+	return flat
+
+def flattenLast(arr):
+	flat = np.zeros((len(arr[:]), len(arr[0][:]), len(arr[0][0][:])*len(arr[0][0][0][:])))
+	for i, x in enumerate(arr):
+		for j, y in enumerate(x):
+			#print(y)
+			flat[i][j] = flatten(y)
+	return flat
 
 def flatten(array):
 	return np.array(array).flatten()
+
+def reconstructPatches(model, features):
+	return [np.dot(x, model.components_) for x in features]
 
 # Returns the dictionary-based model of the provided shapes.
 #Also adds the patches and components to the shape dict
 def getModel(shapes, size=(9,9)):
 	for shape in shapes:
-		shape['patches'] = patches.toPatches(shape['img'], size)
+		shape['patches'] = patcher.toDensePatches(shape['img'], size)
 
-	model = MiniBatchDictionaryLearning()
-	model.fit([flatten(p) for p in shape['patches'] for shape in shapes])
-
-	for shape in shapes:
-		shape['components'] = model.transform([flatten(p) for p in shape['patches']])
+#	model = FastICA(n_components = numComponents)
+	model = PCA(n_components = numComponents)
+	patches = [shape['patches'] for shape in shapes]
+	print('Patches shape: {0}'.format(np.array(patches).shape))
+	flat = flattenLast(patches)
+	print('Flattened shape: {0}'.format(flat.shape))
+	model.fit(flattenFirst(flat))
+	
+	for i, shape in enumerate(shapes):
+		shape['components'] = model.transform(flat[i])
 
 	return model
 
@@ -25,12 +54,38 @@ def shapeFeature(name):
 
 def featurize(shapes, size=(9,9)):
 	model = getModel(shapes, size)
-
-	features = []
-	for shape in shapes:
-		semiglobalFeatures = (shapeFeature(shape['shape'])
-			+ [shape['x'], shape['y'], shape['size']])
-		features.append([components for components in shape['components']]
-			+semiglobalFeatures)
+	x, y = size[0], size[1]
+	features = np.zeros((len(shapes), len(shapes[0]['patches']), numFeatures))
+	for i, shape in enumerate(shapes):
+		semiglobalFeatures = (shapeFeature(shape['shape']) +
+			[shape['x'], shape['y'], shape['size']])
+		shapeComponents = np.concatenate((shape['components'],
+			([semiglobalFeatures]*len(shape['components']))), axis=1)
+		features[i] = shapeComponents
 
 	return model, features
+
+#import shapeGenerator as sg
+#from sklearn.metrics import mean_squared_error
+#circs = sg.randomCircles(1)
+#patches = flattenLast([patcher.toDensePatches(circs[0]['img'])])
+#dimredModel, features = featurize(circs)
+#reconstructedPatches = reconstructPatches(dimredModel, np.reshape(features[:,:,0:numComponents], (-1, numComponents)))
+#print('MSE2')
+#patches = np.reshape(patches, (len(patches[0]), -1))
+#print(np.array(patches).shape)
+#print(np.array(reconstructedPatches).shape)
+#print(mean_squared_error(patches, reconstructedPatches))
+#
+#finalImg = patcher.patchesToImg(reconstructedPatches)
+#print('MSE3')
+#print(mean_squared_error(circs[0]['img'], finalImg))
+#
+#import matplotlib.pyplot as plt
+#plt.figure(figsize=(4.2, 4))
+#plt.subplot(1, 2, 1)
+#plt.imshow(circs[0]['img'], cmap='Greys_r', vmin=0, vmax=1)
+#plt.subplot(1, 2, 2)
+#plt.imshow(finalImg, cmap='Greys_r', vmin=0)
+#plt.show()
+
--- learner.py
+++ learner.py
@@ -1,97 +1,145 @@
 # LSTM for international airline passengers problem with memory
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas
 import math
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
 #Theano flags
-import theano
-theano.config.device = 'gpu'
-theano.config.floatX = 'float32'
+#import theano
+#theano.config.device = 'gpu'
+#theano.config.floatX = 'float32'
 
 ##TJ
-import shapes
+import shapeGenerator
 import featurize
+import patcher
+
+look_back = 20
+batch_size = 3005 # number of patches per image ##TJ
 
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=1):
-	dataX, dataY = [], []
-	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
-		dataX.append(a)
-		dataY.append(dataset[i + look_back, 0])
-	return numpy.array(dataX), numpy.array(dataY)
+	fullX, fullY = [], []
+	print('Creating dataset')
+	for shape in dataset:
+		dataX, dataY = [], []
+		for i in range(len(shape)-look_back):
+			dataX.append(shape[i:(i+look_back), :])
+			dataY.append(shape[i + look_back, :])
+		fullX.append(dataX)
+		fullY.append(dataY)
+	return np.array(fullX), np.array(fullY)
 
 # fix random seed for reproducibility
-numpy.random.seed(7)
+np.random.seed(7)
 
-dataset = shapes.randomSquares(1000)
-model, features = featurize.featurize(dataset)
-
-# normalize the dataset
-##TJ May not scale properly for things like radius so consider changing
-scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
+dataset = shapeGenerator.randomSquares(25) + shapeGenerator.randomCircles(25)
+dimredModel, features = featurize.featurize(dataset)
+features = np.array(features)
 
 # split into train and test sets
-train_size = int(len(dataset) * 0.67)
+train_size = int(round(len(dataset) * 0.67))
 test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print('Train {0}, test {1}'.format(train_size, test_size))
+train, test = features[0:train_size,:], features[train_size:len(features),:]
 
 # reshape into X=t and Y=t+1
-look_back = 20
+print('Z')
+print(train.shape)
 trainX, trainY = create_dataset(train, look_back)
+print(trainX.shape)
+print(trainY.shape)
 testX, testY = create_dataset(test, look_back)
 
-# reshape input to be [samples, time steps, features]
-trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
-testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+print('F')
 
 # create and fit the LSTM network
-batch_size = 63-9 # image size - window size ##TJ
 model = Sequential()
 ##TJ Expected at least 2 hidden layers but preliminary testing to be done with 1
 #model.add(LSTM(100, batch_input_shape=(batch_size, look_back, 1), stateful=True, return_sequences=True))
-model.add(LSTM(100, batch_input_shape=(batch_size, look_back, 1), stateful=True))
-model.add(Dense(81))
+model.add(LSTM(50, batch_input_shape=(1, look_back, featurize.numFeatures)))
+model.add(Dense(featurize.numFeatures))
 model.compile(loss='mean_squared_error', optimizer='adam')
 
-for i in range(100):
-	model.fit(trainX, trainY, nb_epoch=1, batch_size=batch_size, verbose=2, shuffle=False)
-	model.reset_states()
+print('G')
+
+iterations = 2
+for i in range(iterations):
+	for j, x in enumerate(trainX):
+		y = trainY[j]
+		for k in range(len(x)):
+			model.fit(np.reshape(x[k], (1, len(x[k]), len(x[k][0]))), np.reshape(y[k], (1, len(y[k]))), nb_epoch=1, batch_size=1, verbose=0, shuffle=False)
+		model.reset_states()
+
+print('H')
 
 # make predictions
-trainPredict = model.predict(trainX, batch_size=batch_size)
-model.reset_states()
-testPredict = model.predict(testX, batch_size=batch_size)
+trainPredict = trainX[0][0]
+trainScore = 0
+for i, x in enumerate(trainX[0]):
+	nextTrain = model.predict(np.reshape(trainPredict, (1, len(trainPredict), len(trainPredict[0]))), batch_size=batch_size)
+	trainPredict[0:look_back-2] = trainPredict[1:look_back-1]
+	trainPredict[look_back-1] = nextTrain
+	trainScore += math.sqrt(mean_squared_error(np.reshape(trainY[0][i], (1, len(trainY[0][i]))), nextTrain))
+print('Train Score: %.2f RMSE' % (trainScore))
 
-# invert predictions
-trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
-testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
+model.reset_states()
+
+testPredict = testX[0][0]
+testScore = 0
+for i, x in enumerate(testX[0]):
+	nextTest = model.predict(np.reshape(testPredict, (1, len(testPredict), len(testPredict[0]))), batch_size=batch_size)
+	testPredict[0:look_back-2] = testPredict[1:look_back-1]
+	testPredict[look_back-1] = nextTest
+	testScore += math.sqrt(mean_squared_error(np.reshape(testY[0][i], (1, len(testY[0][i]))), nextTest))
+	
+model.reset_states()
 
 # calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
 
+# Testing Full Image Generation
+for imgIndex in range(len(testX[0])):
+	predict = testX[0][imgIndex]
+	print(np.array(predict).shape)
+	allPatches = predict[0:look_back-1]
+	for i in range(batch_size+1):
+		nextPatch = model.predict(np.reshape(predict, (1, len(predict), len(predict[0]))), batch_size=1)
+		predict[0:look_back-2] = predict[1:look_back-1]
+		predict[look_back-1] = nextPatch
+		allPatches = np.vstack((allPatches, nextPatch))
+
+	model.reset_states()
+	allPatches = allPatches[:,0:featurize.numComponents]
+	reconstructedPatches = featurize.reconstructPatches(dimredModel, allPatches)
+	finalImg = patcher.patchesToImg(reconstructedPatches)
+	print(np.array(finalImg).shape)
+	try:
+		print(mean_squared_error(reconstructedPatches, dataset[train_size+imgIndex]['patches']))
+	except Exception as ex:
+		print(ex)
+
+	plt.figure(figsize=(4.2, 4))
+	plt.subplot(2, 1, 1)
+	plt.imshow(dataset[train_size+imgIndex]['img'], cmap='Greys_r')
+	plt.subplot(2, 1, 2)
+	plt.imshow(finalImg, cmap='Greys_r', vmin=0)
+	plt.show()
+
 # shift train predictions for plotting
-trainPredictPlot = numpy.empty_like(dataset)
-trainPredictPlot[:, :] = numpy.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-# shift test predictions for plotting
-testPredictPlot = numpy.empty_like(dataset)
-testPredictPlot[:, :] = numpy.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-# plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
-plt.show()
+#trainPredictPlot = np.empty_like(dataset)
+#trainPredictPlot[:, :] = np.nan
+#trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+## shift test predictions for plotting
+#testPredictPlot = np.empty_like(dataset)
+#testPredictPlot[:, :] = np.nan
+#testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+## plot baseline and predictions
+#plt.plot(scaler.inverse_transform(dataset))
+#plt.plot(trainPredictPlot)
+#plt.plot(testPredictPlot)
+#plt.show()
